894e061fdc19376d92da1502cd90728451d3f614
jeff du <breeze.du@gmail.com>
2020-10-21 00:29:04 -0400

    updated Throttling Gateway java solution

--- RealTests/ThrottlingGateway.java
+++ RealTests/ThrottlingGateway.java
@@ -51,38 +51,142 @@ class ResultTG {
         
         //while requestQ01 contains any request not in current second, remove it
         while( requestQ01.peek()!=null && requestQ01.peek() < currTime){
-        	System.out.print(" Q01 remove item: " + requestQ01.peek());
+        	System.out.println(" Q01 remove item: " + requestQ01.peek());
         	requestQ01.poll();
 
         }
 
         //while requestQ10 contains any request not in past 10 seconds, remove it
         while( requestQ10.peek()!=null && requestQ10.peek() < preTime10 ){
-        	System.out.print(" Q10 remove item: " + requestQ10.peek());
+        	System.out.println(" Q10 remove item: " + requestQ10.peek());
         	requestQ10.poll();
         }
         
         //while requestQ60 contains any request not in past 60 second, remove it
         while( requestQ60.peek()!=null && requestQ60.peek() < preTime60){
-        	System.out.print(" Q60 remove item: " + requestQ60.peek());
+        	System.out.println(" Q60 remove item: " + requestQ60.peek());
         	requestQ60.poll();
         }
         
+        /*****************
+         * Although, if a request is to be dropped due to multiple violations, it is still counted only once.
+         * 27
+1
+1
+1
+1
+2
+2
+2
+3
+3
+3
+4
+4
+4
+5
+5
+5
+6
+6
+6
+7
+7
+7
+7
+11
+11
+11
+11
+ working on ith: 0 currTime=1 pre01=1 pre10=1 pre60=1 Qs size: 0 | 0 | 0
+ working on ith: 1 currTime=1 pre01=1 pre10=1 pre60=1 Qs size: 1 | 1 | 1
+ working on ith: 2 currTime=1 pre01=1 pre10=1 pre60=1 Qs size: 2 | 2 | 2
+ working on ith: 3 currTime=1 pre01=1 pre10=1 pre60=1 Qs size: 3 | 3 | 3
+* Drop 01 
+ working on ith: 4 currTime=2 pre01=1 pre10=1 pre60=1 Qs size: 4 | 4 | 4
+ Q01 remove item: 1
+ Q01 remove item: 1
+ Q01 remove item: 1
+ Q01 remove item: 1
+ working on ith: 5 currTime=2 pre01=1 pre10=1 pre60=1 Qs size: 1 | 5 | 5
+ working on ith: 6 currTime=2 pre01=1 pre10=1 pre60=1 Qs size: 2 | 6 | 6
+ working on ith: 7 currTime=3 pre01=2 pre10=1 pre60=1 Qs size: 3 | 7 | 7
+ Q01 remove item: 2
+ Q01 remove item: 2
+ Q01 remove item: 2
+ working on ith: 8 currTime=3 pre01=2 pre10=1 pre60=1 Qs size: 1 | 8 | 8
+ working on ith: 9 currTime=3 pre01=2 pre10=1 pre60=1 Qs size: 2 | 9 | 9
+ working on ith: 10 currTime=4 pre01=3 pre10=1 pre60=1 Qs size: 3 | 10 | 10
+ Q01 remove item: 3
+ Q01 remove item: 3
+ Q01 remove item: 3
+ working on ith: 11 currTime=4 pre01=3 pre10=1 pre60=1 Qs size: 1 | 11 | 11
+ working on ith: 12 currTime=4 pre01=3 pre10=1 pre60=1 Qs size: 2 | 12 | 12
+ working on ith: 13 currTime=5 pre01=4 pre10=1 pre60=1 Qs size: 3 | 13 | 13
+ Q01 remove item: 4
+ Q01 remove item: 4
+ Q01 remove item: 4
+ working on ith: 14 currTime=5 pre01=4 pre10=1 pre60=1 Qs size: 1 | 14 | 14
+ working on ith: 15 currTime=5 pre01=4 pre10=1 pre60=1 Qs size: 2 | 15 | 15
+ working on ith: 16 currTime=6 pre01=5 pre10=1 pre60=1 Qs size: 3 | 16 | 16
+ Q01 remove item: 5
+ Q01 remove item: 5
+ Q01 remove item: 5
+ working on ith: 17 currTime=6 pre01=5 pre10=1 pre60=1 Qs size: 1 | 17 | 17
+ working on ith: 18 currTime=6 pre01=5 pre10=1 pre60=1 Qs size: 2 | 18 | 18
+ working on ith: 19 currTime=7 pre01=6 pre10=1 pre60=1 Qs size: 3 | 19 | 19
+ Q01 remove item: 6
+ Q01 remove item: 6
+ Q01 remove item: 6
+ working on ith: 20 currTime=7 pre01=6 pre10=1 pre60=1 Qs size: 1 | 20 | 20
+* Drop 10 
+ working on ith: 21 currTime=7 pre01=6 pre10=1 pre60=1 Qs size: 2 | 21 | 21
+* Drop 10 
+ working on ith: 22 currTime=7 pre01=6 pre10=1 pre60=1 Qs size: 3 | 22 | 22
+* Drop 01 
+* Drop 10 
+ working on ith: 23 currTime=11 pre01=10 pre10=2 pre60=1 Qs size: 4 | 23 | 23
+ Q01 remove item: 7
+ Q01 remove item: 7
+ Q01 remove item: 7
+ Q01 remove item: 7
+ Q10 remove item: 1
+ Q10 remove item: 1
+ Q10 remove item: 1
+ Q10 remove item: 1
+ working on ith: 24 currTime=11 pre01=10 pre10=2 pre60=1 Qs size: 1 | 20 | 24
+* Drop 10 
+ working on ith: 25 currTime=11 pre01=10 pre10=2 pre60=1 Qs size: 2 | 21 | 25
+* Drop 10 
+ working on ith: 26 currTime=11 pre01=10 pre10=2 pre60=1 Qs size: 3 | 22 | 26
+* Drop 01 
+* Drop 10 
+Out: 7
+
+         * 
+         */
         //check size of each Queue
         if(requestQ01.size() > 2){
         	drop01 = true;
+        	System.out.println("* Drop 01 ");
+        	requestQ01.add(currRequest);
+        	
         } else {
         	requestQ01.add(currRequest);
         }
         
         if(requestQ10.size() > 19){
         	drop10 = true;
+        	System.out.println("* Drop 10 ");
+        	requestQ10.add(currRequest);
         } else {
         	requestQ10.add(currRequest);
         }
         
         if(requestQ60.size() > 59){
         	drop60 = true;
+        	System.out.println("* Drop 60 ");
+        	requestQ60.add(currRequest);
         } else {
         	requestQ60.add(currRequest);
         }
