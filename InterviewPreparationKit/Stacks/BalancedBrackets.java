7770f5c3dc45a64d333d799a726e17d39e39e66f
jeff du <breeze.du@gmail.com>
2020-11-30 22:38:10 -0500

    upload stack section checking if a string of brackets match

--- InterviewPreparationKit/Stacks/BalancedBrackets.java
+++ InterviewPreparationKit/Stacks/BalancedBrackets.java
@@ -71,7 +71,7 @@ public class BalancedBrackets {
 	private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        // BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
         int t = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
@@ -79,13 +79,15 @@ public class BalancedBrackets {
         for (int tItr = 0; tItr < t; tItr++) {
             String s = scanner.nextLine();
 
-            String result = isBalanced(s);
+            String result = isBalanced(s); 
+            
+            System.out.println(" The brackets: " + s + " is " + result + " balanced"); 
 
-            bufferedWriter.write(result);
-            bufferedWriter.newLine();
+            // bufferedWriter.write(result);
+            // bufferedWriter.newLine();
         }
 
-        bufferedWriter.close();
+        // bufferedWriter.close();
 
         scanner.close();
     }
